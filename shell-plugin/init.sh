c5a9ff9a1d9ec50f51fc44fe2a27a955a833d301
Gonzalo Matheu <gonzalommj@gmail.com>
2016-01-12 22:32:10 -0300

    Adding install-theme function

--- shell-plugin/init.sh
+++ shell-plugin/init.sh
@@ -4,3 +4,7 @@ DOTFILES_HOME=${DOTFILES_HOME:="$HOME/.dotfiles"}
 update-dotfiles() {
   sh "$DOTFILES_HOME/install.sh"
 }
+
+install-terminal-theme() {
+  curl -s "https://raw.githubusercontent.com/chriskempson/base16-gnome-terminal/master/base16-3024.dark.sh" | bash
+}
