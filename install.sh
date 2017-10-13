8c40a48f0bcac9974f09b925dc12053b17fbebeb
Gonzalo Matheu <gmatheu@ascentio.com.ar>
2017-10-13 10:40:51 -0300

    Forcing dependencies install when bootstrapping

--- install.sh
+++ install.sh
@@ -59,23 +59,27 @@ check_function() {
 
 link_file() {
   local file="$1"
-  ln -fs $DOTFILES_FILES/$file $HOME/.$file && info "$file linked" 
+  ln -fs $DOTFILES_FILES/$file $HOME/.$file && info "$file linked"
 }
 
 setup_tmux() {
+  sudo add-apt-repository -y ppa:pi-rho/dev &&\
+  sudo apt-get install -yqq python-software-properties software-properties-common &&\
+  sudo apt-get update -yqq &&\
+  sudo apt install -yqq tmux
   local bin="tmux"
   info "Setting up $bin"
   local exists=$(check_bin $bin)
   if [ "$exists" = "true" ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || echo 'tpm already there'
-    link_file "tmux.conf" 
+    link_file "tmux.conf"
   fi
 }
 
 configure_zsh() {
     if [ -z $ZSH ]; then
       info "Setting $bin as default shell"
-      sudo chsh -s `pwd`
+      chsh -s `which zsh`
     fi
 }
 
@@ -84,19 +88,20 @@ setup_zsh() {
   info "Setting up $bin"
   local exists=$(check_bin $bin)
   if [ "$exists" = "true" ]; then
-    link_file "zshrc" 
-    link_file "zshenv" 
+    link_file "zshrc"
+    link_file "zshenv"
     configure_zsh
   fi
 }
 
 configure_vim() {
-  # sudo apt-get install build-essential cmake python-dev
+  sudo apt install -yqq vim cmake
   cd "$VIM_HOME" && ./scripts/setup
   "$VIM_HOME"/bundle/YouCompleteMe/install.py
 }
 
 setup_vim() {
+  sudo apt install -yqq cmake
   local bin="vim"
   info "Setting up $bin"
   local exists=$(check_bin $bin)
