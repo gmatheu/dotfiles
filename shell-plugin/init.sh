f7d36427b67b98d219a5dc5572e014b408e2584a
Gonzalo Matheu <gonzalommj@gmail.com>
2018-04-08 18:32:36 -0300

    Removing install-theme from plugin

--- shell-plugin/init.sh
+++ shell-plugin/init.sh
@@ -1,10 +1,7 @@
 #! /bin/sh
 DOTFILES_HOME=${DOTFILES_HOME:="$HOME/.dotfiles"}
 
-update-dotfiles() {
+update_dotfiles() {
   sh "$DOTFILES_HOME/install.sh"
 }
 
-install-terminal-theme() {
-  curl -s "https://raw.githubusercontent.com/chriskempson/base16-gnome-terminal/master/base16-3024.dark.sh" | bash
-}
