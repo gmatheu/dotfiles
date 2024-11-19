effa02fadf5b44e763a18f663aacea7d017a62cc
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2024-11-19 16:34:26 -0300

    nix/zshrc: Enabling carapache completion

--- stow-files/home-manager/home.nix
+++ stow-files/home-manager/home.nix
@@ -82,4 +82,10 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.carapace = {
+    enable = true;
+  };
+
+  # programs.zsh.enable = true;
 }
