30095d7d4aa3e7bbec25eac3a39cf28cbe915ec9
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2024-12-03 14:30:31 -0300

    nix/tmux: Adding tmux scripts to home-manager

--- home-manager-files/tmux/scripts/move-windows-default-stats.sh
+++ home-manager-files/tmux/scripts/move-windows-default-stats.sh
@@ -1,15 +1,4 @@
 #! /bin/bash
 
-tmux split-window -h -d
-tmux split-window -h -d
-tmux split-window -h -d
-
-tmux select-layout main-vertical
-
-tmux send-keys -t 4 nvtop Enter
-tmux send-keys -t 4 Enter
-
-tmux send-keys -t 3 'pipx run nvitop' Enter
-
-tmux send-keys -t 2 'ollama serve' Enter
-tmux resize-pane -t 2 -U 4
+tmux move-window -s 3 -t 8
+tmux move-window -s 4 -t 9
