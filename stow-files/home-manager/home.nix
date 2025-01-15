c41c4dfcb65ab88e83c14b43cb7622156c960d93
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2025-01-15 10:44:01 -0300

    kanata: Adding kanata config files

--- stow-files/home-manager/home.nix
+++ stow-files/home-manager/home.nix
@@ -26,6 +26,7 @@ in {
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
+    pkgs.nixpkgs-fmt
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
