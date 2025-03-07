e1d1e635fbb8ae9c36dbf9359d897da29e2bde91
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2025-03-07 14:41:32 -0300

    intelli-shell: Fixing docker stop all

--- stow-files/intelli-shell/scripts/docker_fzf_stop_all.sh
+++ stow-files/intelli-shell/scripts/docker_fzf_stop_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ## Stop all running containers
-docker ps | fzf --multi --prompt 'Enter to stop container / Ctrl-a: select all / Ctrl-d: deselect all / Ctrl-t: toggle > ' --bind 'ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all,enter:become(echo {1})' | xargs -I{} docker stop {}
+docker ps | fzf --multi --prompt 'Enter to stop container / Ctrl-a: select all / Ctrl-d: deselect all / Ctrl-t: toggle > ' --bind 'ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all' | cut -d ' ' -f 1 | xargs -I{} docker stop {}
