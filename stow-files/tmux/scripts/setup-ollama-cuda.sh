c31e0eba42020d8c55d4958a6e3dd72d2f8075d6
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2025-03-07 10:14:05 -0300

    eget: Adding ollama and zed

--- stow-files/tmux/scripts/setup-ollama-cuda.sh
+++ stow-files/tmux/scripts/setup-ollama-cuda.sh
@@ -9,6 +9,7 @@ tmux select-layout main-vertical
 tmux send-keys -t 4 nvtop Enter
 tmux send-keys -t 4 Enter
 
+tmux send-keys -t 1 'nvidia-smi' Enter
 tmux send-keys -t 3 'pipx run nvitop' Enter
 
 tmux send-keys -t 2 'ollama serve' Enter
