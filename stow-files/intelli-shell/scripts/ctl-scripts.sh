4ed9b452bb997a8d00f7fca1261b4deb56a5abd6
Gonzalo Matheu (framework) <gonzalommj@gmail.com>
2025-03-14 17:19:48 -0300

    inteli-shell: Adding executed command to history on ctl-scripts

--- stow-files/intelli-shell/scripts/ctl-scripts.sh
+++ stow-files/intelli-shell/scripts/ctl-scripts.sh
@@ -5,4 +5,4 @@ find -L ${SCRIPTS_HOME} -name '*.sh' -printf '%f\n' |
   fzf \
     --prompt "ctrl-e: edit / Enter: execute > " \
     --preview "cat ${SCRIPTS_HOME}{}" \
-    --bind "ctrl-e:execute($EDITOR ${SCRIPTS_HOME}{}),enter:become(bash ${SCRIPTS_HOME}{})"
+    --bind "ctrl-e:execute($EDITOR ${SCRIPTS_HOME}{}),enter:execute-silent(atuin history start "${SCRIPTS_HOME}{}")+execute-silent(print -s "${SCRIPTS_HOME}{}")+become(bash ${SCRIPTS_HOME}{})"
