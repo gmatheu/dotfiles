d2dd19e75f89733261aca75d88d8adcee40625ef
Gonzalo Matheu (framework) <gonzalo.matheu@intive.com>
2026-01-27 12:44:51 -0300

    Updating submodules references

--- bootstrap.nix.sh
+++ bootstrap.nix.sh
@@ -1,8 +1,6 @@
-
 sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
 
-
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
+export PATH=$PATH:$HOME/.nix-profile/bin
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
-
